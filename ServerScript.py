eb634f6471e9c5c73dd28d07a6dc989d63c49b3e
AlexBMJ <AlexBMJ@users.noreply.github.com>
2018-10-26 13:38:00 +0200

    Fixed database solution for server

--- ServerScript.py
+++ ServerScript.py
@@ -1,28 +1,66 @@
 from multiplayer import ThreadedServer
 
-class Database:
-	def __init__(self, username=None, parent=None):
+class Object:
+	def __init__(self, username=None, type=None, parent=None, position=(0,0), angle=0, fired=False):
 		self.username = username
+		self.position = position
+		self.angle = angle
+		self.type = type
+		self.fired = fired
 		self.parent = parent
-		self.players = []
-		self.player_data = {}
+		self.subObjects=[]
 
-	def addPlayer(self, object, input={}):
+	def create(self, object, input={}):
 		self.subObjects.append(object(parent=self, **input))
-		return self.players[-1]
+		return self.subObjects[-1]
 
-	def deletePlayer(self, object=None):
-		if object == None:
-			for obj in players:
-				pass
+	def delete(self, string=''):
+		if string=='':
+			string=str(self.parent)
 
+		for obj in self.subObjects:
+			obj.delete(string)
+		
+		if self.parent!=None:
+			for index, obj in enumerate(self.parent.subObjects):
+				if obj==self:
+					self.parent.subObjects.pop(index)
+					if self.parent.subObjects!=[] and str(self.parent)!=string:
+						self.parent.subObjects[0].delete(string)
 
+	def find(self, username=None, type=None):
+		if username == None and type == None:
+			raise error('find() needs an input')
+		elif username != None:
+			for object in self.subObjects:
+				if object.username==username:
+					return object
+			return False
+		elif type != None:
+			objecttype=[]
+			for object in self.subObjects:
+				if object.type==type:
+					objecttype.append(object)
+			return objecttype
 
+class Database(Object):
+	def __init__(self):
+		super().__init__()
+		pass
 
-server_data = {'player_data':{}}
+class Player(Object):
+	def __init__(self, username, parent):
+		super().__init__(
+			username=username,
+			type='Player',
+			position=(0,0),
+			angle=0,
+			fired=False,
+			parent=parent)
 
 address_id = {}
 
+database = Database()
 
 def server_started():
 	print('Server is running!')
@@ -42,32 +80,32 @@ def event_handler(raw_json):
 			return False
 
 	if 'start_connection' in list(event_data.keys()):
-		if event_data['start_connection']['username'] in [username[0] for username in list(address_id.values())]:
-			for user in [password for password in list(address_id.values())]:
-				if event_data['start_connection']['username'] == user[0]:
-					if event_data['start_connection']['password'] == user[1]:
-						player_ref = event_data['start_connection']['username']
-						return str({'start_connection':server_data['player_data'][player_ref]})
-					else:
-						return str({'connection_denied':'wrong_password'})
+		if event_data['start_connection']['username'] in list(address_id.values()):
+			for user in list(address_id.values()):
+				if event_data['start_connection']['username'] == user:
+					player_ref = database.find(username=user)
+					return str({'start_connection':{'position':player_ref.position}})
+				else:
+					return str({'connection_denied':'wrong_password'})
 		else:
-			address_id[address] = (event_data['start_connection']['username'],event_data['start_connection']['password'])
-			player_ref = event_data['start_connection']['username']
-			server_data['player_data'][player_ref] = {'position':(0,0),'angle':90}
+			address_id[address] = event_data['start_connection']['username']
+			database.create(Player, {'username':user})
+			player_ref = database.find(username=user)
 			print('User Created!')
-			return str({'start_connection':server_data['player_data'][player_ref]})
+			return str({'start_connection':{'position':player_ref.position}})
 	else:
-		for user in address_id:
-			if address == user:
-				player_ref = address_id[user][0]
+		for user_ip in address_id:
+			if address == user_ip:
+				player_ref = database.find(username=address_id[user_ip])
 
 	for event in event_data['player_data']:
-		server_data['player_data'][player_ref][event] = event_data['player_data'][event]
+		setattr(player_ref, event, event_data['player_data'][event])
 
 	send_data = {'player_data':{}}
-	for user in server_data['player_data']:
-		if user != player_ref and user in [username[0] for username in list(address_id.values())]:
-			send_data['player_data'][user] = server_data['player_data'][user]
+	for user in list(address_id.values()):
+		if user != player_ref.username:
+			temp_user = database.find(username=user)
+			send_data['player_data'][user] = {'position':temp_user.position, 'angle':temp_user.angle, 'fired':temp_user.fired}
 	return str(send_data)
 
 
