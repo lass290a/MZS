3b98a78f70599ea942a9fa4f4d0c16eb0bc5ed39
lass290a <31809070+lass290a@users.noreply.github.com>
2019-01-25 20:56:53 +0100

    Events for engine

--- benchmark.py
+++ benchmark.py
@@ -1,4 +1,3 @@
-import arcade
 import engine as engine
 from random import choice, uniform
 from math import cos, sin
@@ -14,11 +13,11 @@ class Game(engine.Game):
 			background_color=(0, 0, 0))
 		self.average_frames = []
 
-	def update(self, delta_time):
+	def event_update(self, delta_time):
 		self.average_frames.append(1/delta_time)
 		if self.frame < 100:
 			world.create(Ball,
-				sprite_filename="ball1",
+				sprite="ball1",
 				layer="main",
 				width=25,
 				height=25,
@@ -30,29 +29,28 @@ class Game(engine.Game):
 				change_x=uniform(-4, 4),
 				elasticity=0.9)
 
-	def on_close(self):
-		arcade.window_commands.close_window()
-
-	def on_mouse_motion(self, x, y, dx, dy):
+	def event_close(self):
 		pass
 
-	def on_mouse_press(self, x, y, button, modifiers):
+	def event_mouse_motion(self, x, y, dx, dy):
 		pass
 
-	def on_mouse_release(self, x, y, button, modifiers):
+	def event_mouse_press(self, x, y, button, modifiers):
+		print(button, modifiers)
+
+	def event_mouse_release(self, x, y, button, modifiers):
+		print(button, modifiers)
+
+	def event_key_press(self, key, modifiers):
 		pass
 
-	def on_key_press(self, key, modifiers):
-		if key == arcade.key.LSHIFT:
-			print("YO") 
-
-	def on_key_release(self, key, modifiers):
+	def event_key_release(self, key, modifiers):
 		pass
 
 class Ball(engine.Sprite):
-	def __init__(self, parent, sprite_filename, layer, width=50, height=50, x=0, y=0, change_x=0, change_y=0, gravity_x=0, gravity_y=-1, elasticity=0.9):
+	def __init__(self, parent, sprite, layer, width=50, height=50, x=0, y=0, change_x=0, change_y=0, gravity_x=0, gravity_y=-1, elasticity=0.9):
 		super().__init__(parent=parent,
-			sprite_filename=sprite_filename,
+			sprite=sprite,
 			layer=layer,
 			width=width,
 			height=height,
@@ -65,7 +63,7 @@ class Ball(engine.Sprite):
 		self.change_y = change_y
 		self.elasticity = elasticity
 
-	def update(self):
+	def event_update(self):
 		self.change_x += self.gravity_x
 		self.change_y += self.gravity_y
 		self.x += self.change_x
@@ -94,7 +92,7 @@ class Ball(engine.Sprite):
 class Orbit(engine.Sprite):
 	def __init__(self, parent):
 		super().__init__(parent=parent,
-			sprite_filename="ball3.png",
+			sprite="ball3.png",
 			layer="main",
 			width=50,
 			height=50)
@@ -102,7 +100,7 @@ class Orbit(engine.Sprite):
 		self.orbit_speed = 0.1
 		self.orbit_radius = 25
 
-	def update(self):
+	def event_update(self):
 		self.orbit_angle += self.orbit_speed
 		self.x = cos(self.orbit_angle)*self.orbit_radius
 		self.y = sin(self.orbit_angle)*self.orbit_radius
@@ -111,5 +109,4 @@ game = Game()
 world = game.create(engine.Entity)
 game.world = world
 
-print(arcade.window_commands.get_window().__class__)
-arcade.run()
+game.start()
--- engine.py
+++ engine.py
@@ -4,7 +4,14 @@ from inspect import getmro
 import glob, os
 from pyglet import gl
 
-key_codes = {65288: 'motion_backspace', 65289: 'tab', 65290: 'linefeed', 65291: 'clear', 65293: 'enter', 65299: 'pause', 65300: 'scrolllock', 65301: 'sysreq', 65307: 'escape', 65360: 'home', 65361: 'motion_left', 65362: 'motion_up', 65363: 'motion_right', 65364: 'motion_down', 65365: 'motion_previous_page', 65366: 'motion_next_page', 65367: 'end', 65368: 'begin', 65535: 'motion_delete', 65376: 'select', 65377: 'print', 65378: 'execute', 65379: 'insert', 65381: 'undo', 65382: 'redo', 65383: 'menu', 65384: 'find', 65385: 'cancel', 65386: 'help', 65387: 'break', 65406: 'scriptswitch', 1: 'motion_next_word', 2: 'motion_previous_word', 3: 'motion_beginning_of_line', 4: 'motion_end_of_line', 5: 'motion_beginning_of_file', 6: 'motion_end_of_file', 65407: 'numlock', 65408: 'num_space', 65417: 'num_tab', 65421: 'num_enter', 65425: 'num_f1', 65426: 'num_f2', 65427: 'num_f3', 65428: 'num_f4', 65429: 'num_home', 65430: 'num_left', 65431: 'num_up', 65432: 'num_right', 65433: 'num_down', 65434: 'num_page_up', 65435: 'num_page_down', 65436: 'num_end', 65437: 'num_begin', 65438: 'num_insert', 65439: 'num_delete', 65469: 'num_equal', 65450: 'num_multiply', 65451: 'num_add', 65452: 'num_separator', 65453: 'num_subtract', 65454: 'num_decimal', 65455: 'num_divide', 65456: 'num_0', 65457: 'num_1', 65458: 'num_2', 65459: 'num_3', 65460: 'num_4', 65461: 'num_5', 65462: 'num_6', 65463: 'num_7', 65464: 'num_8', 65465: 'num_9', 65470: 'f1', 65471: 'f2', 65472: 'f3', 65473: 'f4', 65474: 'f5', 65475: 'f6', 65476: 'f7', 65477: 'f8', 65478: 'f9', 65479: 'f10', 65480: 'f11', 65481: 'f12', 65482: 'f13', 65483: 'f14', 65484: 'f15', 65485: 'f16', 65505: 'lshift', 65506: 'rshift', 65507: 'lctrl', 65508: 'rctrl', 65509: 'capslock', 65511: 'lmeta', 65512: 'rmeta', 65513: 'lalt', 65514: 'ralt', 65515: 'lwindows', 65516: 'rwindows', 65517: 'lcommand', 65518: 'rcommand', 65488: 'loption', 65489: 'roption', 32: 'space', 33: 'exclamation', 34: 'doublequote', 35: 'pound', 36: 'dollar', 37: 'percent', 38: 'ampersand', 39: 'apostrophe', 40: 'parenleft', 41: 'parenright', 42: 'asterisk', 43: 'plus', 44: 'comma', 45: 'minus', 46: 'period', 47: 'slash', 48: 'key_0', 49: 'key_1', 50: 'key_2', 51: 'key_3', 52: 'key_4', 53: 'key_5', 54: 'key_6', 55: 'key_7', 56: 'key_8', 57: 'key_9', 58: 'colon', 59: 'semicolon', 60: 'less', 61: 'equal', 62: 'greater', 63: 'question', 64: 'at', 91: 'bracketleft', 92: 'backslash', 93: 'bracketright', 94: 'asciicircum', 95: 'underscore', 96: 'quoteleft', 97: 'a', 98: 'b', 99: 'c', 100: 'd', 101: 'e', 102: 'f', 103: 'g', 104: 'h', 105: 'i', 106: 'j', 107: 'k', 108: 'l', 109: 'm', 110: 'n', 111: 'o', 112: 'p', 113: 'q', 114: 'r', 115: 's', 116: 't', 117: 'u', 118: 'v', 119: 'w', 120: 'x', 121: 'y', 122: 'z', 123: 'braceleft', 124: 'bar', 125: 'braceright', 126: 'asciitilde'}
+class Mouse:
+	def __init__(self):
+		self.x = 0
+		self.y = 0
+		self.dx = 0
+		self.dy = 0
+		self.click = 0
+		self.modifier = 0
 
 def pos_to_ang(x, y):
 	try:
@@ -34,10 +41,6 @@ class Entity:
 	def create(self, Object, **kwargs):
 		obj = Object(parent=self, **kwargs)
 		self.children.append(obj)
-		"""try:
-			self.origin.sprite_list.insert(obj)
-		except AttributeError:
-			pass"""
 		return obj
 
 	def find(self, condition=lambda x:x, **kwargs):
@@ -93,30 +96,17 @@ class SpriteList(arcade.SpriteList):
 		self.layerIndicies = {layer:0 for layer in layers}
 
 	def insert(self, obj):
-		print(obj, self.sprite_list)
 		if obj.overlap_parent != None:
 			self.sprite_list.insert(self.sprite_list.index(obj.overlap_parent)+1, obj)
 		else:
 			self.sprite_list.insert(self.layerIndicies[obj.layer], obj)
 		for layer in list(self.layerIndicies.keys())[list(self.layerIndicies.keys()).index(obj.layer):]:
 			self.layerIndicies[layer] += 1
-		print(self.layerIndicies)
 
 	def remove(self, obj):
 		self.sprite_list.remove(obj)
 		for layer in list(self.layerIndicies.keys())[list(self.layerIndicies.keys()).index(obj.layer):]:
 			self.layerIndicies[layer] -= 1
-	
-	"""def replace(self, target, new_object):
-		obj_idx = None
-		for index, obj in enumerate(self.sprite_list):
-			if obj == target:
-				obj_idx = index
-		if obj_idx == None:
-			raise Exception(f'unable to find object "{target}" in sprite list')
-		
-		self.sprite_list.pop(obj_idx)
-		self.sprite_list.insert((obj_idx), object)"""
 
 class Game(Entity, arcade.Window):
 	def __init__(self, width, height, sprites_folder_path, layers=['gnd', 'gtop', 'main', 'top'], update_rate=1/60, render_scale=1, background_color=(0, 0, 0)):
@@ -132,6 +122,35 @@ class Game(Entity, arcade.Window):
 		self.background_color = background_color
 		self.frame = 0
 		
+	def update(self, delta_time):
+		if "event_update" in dir(self): self.event_update(delta_time)
+
+	def on_close(self):
+		if "event_close" in dir(self): self.event_close()
+		arcade.window_commands.close_window()
+
+	def on_mouse_motion(self, x, y, dx, dy):
+		if "event_mouse_motion" in dir(self): self.event_mouse_motion(x, y, dx, dy)
+		mouse.x = x
+		mouse.y = y
+		mouse.dx = dx
+		mouse.dy = dy
+
+	def on_mouse_press(self, x, y, button, modifiers):
+		if "event_mouse_press" in dir(self): self.event_mouse_press(x, y, button, modifiers)
+
+	def on_mouse_release(self, x, y, button, modifiers):
+		if "event_mouse_release" in dir(self): self.event_mouse_release(x, y, button, modifiers)
+
+	def on_key_press(self, key, modifiers):
+		if "event_key_press" in dir(self): self.event_key_press(key, modifiers)
+
+	def on_key_release(self, key, modifiers):
+		if "event_key_release" in dir(self): self.event_key_release(key, modifiers)
+
+	def start(self):
+		arcade.run()
+
 	def on_draw(self):
 		self.frame += 1
 		
@@ -150,6 +169,10 @@ class Game(Entity, arcade.Window):
 		arcade.start_render()
 		self.sprite_list.draw()
 		gl.glFlush()
+
+mouse = Mouse()
+
+key_codes = {65288: 'motion_backspace', 65289: 'tab', 65290: 'linefeed', 65291: 'clear', 65293: 'enter', 65299: 'pause', 65300: 'scrolllock', 65301: 'sysreq', 65307: 'escape', 65360: 'home', 65361: 'motion_left', 65362: 'motion_up', 65363: 'motion_right', 65364: 'motion_down', 65365: 'motion_previous_page', 65366: 'motion_next_page', 65367: 'end', 65368: 'begin', 65535: 'motion_delete', 65376: 'select', 65377: 'print', 65378: 'execute', 65379: 'insert', 65381: 'undo', 65382: 'redo', 65383: 'menu', 65384: 'find', 65385: 'cancel', 65386: 'help', 65387: 'break', 65406: 'scriptswitch', 1: 'motion_next_word', 2: 'motion_previous_word', 3: 'motion_beginning_of_line', 4: 'motion_end_of_line', 5: 'motion_beginning_of_file', 6: 'motion_end_of_file', 65407: 'numlock', 65408: 'num_space', 65417: 'num_tab', 65421: 'num_enter', 65425: 'num_f1', 65426: 'num_f2', 65427: 'num_f3', 65428: 'num_f4', 65429: 'num_home', 65430: 'num_left', 65431: 'num_up', 65432: 'num_right', 65433: 'num_down', 65434: 'num_page_up', 65435: 'num_page_down', 65436: 'num_end', 65437: 'num_begin', 65438: 'num_insert', 65439: 'num_delete', 65469: 'num_equal', 65450: 'num_multiply', 65451: 'num_add', 65452: 'num_separator', 65453: 'num_subtract', 65454: 'num_decimal', 65455: 'num_divide', 65456: 'num_0', 65457: 'num_1', 65458: 'num_2', 65459: 'num_3', 65460: 'num_4', 65461: 'num_5', 65462: 'num_6', 65463: 'num_7', 65464: 'num_8', 65465: 'num_9', 65470: 'f1', 65471: 'f2', 65472: 'f3', 65473: 'f4', 65474: 'f5', 65475: 'f6', 65476: 'f7', 65477: 'f8', 65478: 'f9', 65479: 'f10', 65480: 'f11', 65481: 'f12', 65482: 'f13', 65483: 'f14', 65484: 'f15', 65485: 'f16', 65505: 'lshift', 65506: 'rshift', 65507: 'lctrl', 65508: 'rctrl', 65509: 'capslock', 65511: 'lmeta', 65512: 'rmeta', 65513: 'lalt', 65514: 'ralt', 65515: 'lwindows', 65516: 'rwindows', 65517: 'lcommand', 65518: 'rcommand', 65488: 'loption', 65489: 'roption', 32: 'space', 33: 'exclamation', 34: 'doublequote', 35: 'pound', 36: 'dollar', 37: 'percent', 38: 'ampersand', 39: 'apostrophe', 40: 'parenleft', 41: 'parenright', 42: 'asterisk', 43: 'plus', 44: 'comma', 45: 'minus', 46: 'period', 47: 'slash', 48: 'key_0', 49: 'key_1', 50: 'key_2', 51: 'key_3', 52: 'key_4', 53: 'key_5', 54: 'key_6', 55: 'key_7', 56: 'key_8', 57: 'key_9', 58: 'colon', 59: 'semicolon', 60: 'less', 61: 'equal', 62: 'greater', 63: 'question', 64: 'at', 91: 'bracketleft', 92: 'backslash', 93: 'bracketright', 94: 'asciicircum', 95: 'underscore', 96: 'quoteleft', 97: 'a', 98: 'b', 99: 'c', 100: 'd', 101: 'e', 102: 'f', 103: 'g', 104: 'h', 105: 'i', 106: 'j', 107: 'k', 108: 'l', 109: 'm', 110: 'n', 111: 'o', 112: 'p', 113: 'q', 114: 'r', 115: 's', 116: 't', 117: 'u', 118: 'v', 119: 'w', 120: 'x', 121: 'y', 122: 'z', 123: 'braceleft', 124: 'bar', 125: 'braceright', 126: 'asciitilde'}
 
 if __name__ == "__main__":
 	print("stop opening the engine.py you retard")
