3eda67536c5e2005b084fce1f527e2b71823c717
meatface <lass290a@gmail.com>
2019-02-01 07:42:03 +0100

    Added Entity.exists

--- engine.py
+++ engine.py
@@ -4,10 +4,6 @@ from inspect import getmro
 import glob, os
 from pyglet import gl
 
-class Keyboard:
-	def __init__(self):
-		self.held_keys = []
-
 class Mouse:
 	def __init__(self):
 		self.x = 0
@@ -31,6 +27,7 @@ class Entity:
 		self.relative_rotation = relative_rotation
 		self.parent = parent
 		self.children = []
+		self.exists = True
 		if self.parent == None:
 			self.origin = self
 		else:
@@ -56,12 +53,12 @@ class Entity:
 			if Sprite in getmro(self.__class__):
 				self.origin.sprite_list.remove(self)
 				self.kill()
+			self.exists = False
 
 	def determine_orientation(self):
 		if self.relative_position and self.parent != self.origin:
 			self.center_x = self.parent.center_x+(cos(radians(self.parent.rotation))*self.x*self.origin.render_scale+cos(radians(self.parent.rotation+90))*self.y*self.origin.render_scale)
 			self.center_y = self.parent.center_y+(sin(radians(self.parent.rotation))*self.x*self.origin.render_scale+sin(radians(self.parent.rotation+90))*self.y*self.origin.render_scale)
-			print(self.center_x)
 		else:
 			self.center_x = self.x
 			self.center_y = self.y
@@ -150,12 +147,12 @@ class Game(Entity, arcade.Window):
 		if "event_mouse_release" in dir(self): self.event_mouse_release(x, y, button, modifiers)
 
 	def on_key_press(self, key, modifiers):
-		if not chr(key) in self.heldKeys:
-			keyboard.held_keys.append(key_codes[key])
+		if not key_codes[key] in held_keys:
+			held_keys.append(key_codes[key])
 		if "event_key_press" in dir(self): self.event_key_press(key, modifiers)
 
 	def on_key_release(self, key, modifiers):
-		del keyboard.held_keys[keyboard.held_keys.index(key_codes[key])]
+		del held_keys[held_keys.index(key_codes[key])]
 		if "event_key_release" in dir(self): self.event_key_release(key, modifiers)
 
 
@@ -182,7 +179,7 @@ class Game(Entity, arcade.Window):
 		gl.glFlush()
 
 mouse = Mouse()
-keyboard = Keyboard()
+held_keys = []
 
 key_codes = {65288: 'motion_backspace', 65289: 'tab', 65290: 'linefeed', 65291: 'clear', 65293: 'enter', 65299: 'pause', 65300: 'scrolllock', 65301: 'sysreq', 65307: 'escape', 65360: 'home', 65361: 'motion_left', 65362: 'motion_up', 65363: 'motion_right', 65364: 'motion_down', 65365: 'motion_previous_page', 65366: 'motion_next_page', 65367: 'end', 65368: 'begin', 65535: 'motion_delete', 65376: 'select', 65377: 'print', 65378: 'execute', 65379: 'insert', 65381: 'undo', 65382: 'redo', 65383: 'menu', 65384: 'find', 65385: 'cancel', 65386: 'help', 65387: 'break', 65406: 'scriptswitch', 1: 'motion_next_word', 2: 'motion_previous_word', 3: 'motion_beginning_of_line', 4: 'motion_end_of_line', 5: 'motion_beginning_of_file', 6: 'motion_end_of_file', 65407: 'numlock', 65408: 'num_space', 65417: 'num_tab', 65421: 'num_enter', 65425: 'num_f1', 65426: 'num_f2', 65427: 'num_f3', 65428: 'num_f4', 65429: 'num_home', 65430: 'num_left', 65431: 'num_up', 65432: 'num_right', 65433: 'num_down', 65434: 'num_page_up', 65435: 'num_page_down', 65436: 'num_end', 65437: 'num_begin', 65438: 'num_insert', 65439: 'num_delete', 65469: 'num_equal', 65450: 'num_multiply', 65451: 'num_add', 65452: 'num_separator', 65453: 'num_subtract', 65454: 'num_decimal', 65455: 'num_divide', 65456: 'num_0', 65457: 'num_1', 65458: 'num_2', 65459: 'num_3', 65460: 'num_4', 65461: 'num_5', 65462: 'num_6', 65463: 'num_7', 65464: 'num_8', 65465: 'num_9', 65470: 'f1', 65471: 'f2', 65472: 'f3', 65473: 'f4', 65474: 'f5', 65475: 'f6', 65476: 'f7', 65477: 'f8', 65478: 'f9', 65479: 'f10', 65480: 'f11', 65481: 'f12', 65482: 'f13', 65483: 'f14', 65484: 'f15', 65485: 'f16', 65505: 'lshift', 65506: 'rshift', 65507: 'lctrl', 65508: 'rctrl', 65509: 'capslock', 65511: 'lmeta', 65512: 'rmeta', 65513: 'lalt', 65514: 'ralt', 65515: 'lwindows', 65516: 'rwindows', 65517: 'lcommand', 65518: 'rcommand', 65488: 'loption', 65489: 'roption', 32: 'space', 33: 'exclamation', 34: 'doublequote', 35: 'pound', 36: 'dollar', 37: 'percent', 38: 'ampersand', 39: 'apostrophe', 40: 'parenleft', 41: 'parenright', 42: 'asterisk', 43: 'plus', 44: 'comma', 45: 'minus', 46: 'period', 47: 'slash', 48: 'key_0', 49: 'key_1', 50: 'key_2', 51: 'key_3', 52: 'key_4', 53: 'key_5', 54: 'key_6', 55: 'key_7', 56: 'key_8', 57: 'key_9', 58: 'colon', 59: 'semicolon', 60: 'less', 61: 'equal', 62: 'greater', 63: 'question', 64: 'at', 91: 'bracketleft', 92: 'backslash', 93: 'bracketright', 94: 'asciicircum', 95: 'underscore', 96: 'quoteleft', 97: 'a', 98: 'b', 99: 'c', 100: 'd', 101: 'e', 102: 'f', 103: 'g', 104: 'h', 105: 'i', 106: 'j', 107: 'k', 108: 'l', 109: 'm', 110: 'n', 111: 'o', 112: 'p', 113: 'q', 114: 'r', 115: 's', 116: 't', 117: 'u', 118: 'v', 119: 'w', 120: 'x', 121: 'y', 122: 'z', 123: 'braceleft', 124: 'bar', 125: 'braceright', 126: 'asciitilde'}
 
